99ac4d07ec0beba75a80c8b6006ba7c09f037915
ihab shahtout <ishahtout@walaplus.com>
2021-11-15 20:38:54 +0200

    add routes invoice and status

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('invoice','PaymentController@CreateLinkRequest')->name('invoice.index');
+Route::post('status','PaymentController@SendInvoiceStatusRequest')->name('invoice.status');
